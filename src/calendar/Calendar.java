fa4439a6c1956b3d0e75c94daaee7807d6d9ae21
kmh <sss848228@gmail.com>
2025-01-30 21:12:38 +0800

    init Calendar pjt

---
+++ src/calendar/Calendar.java
@@ -0,0 +1,12 @@
+package calendar;
+
+public class Calendar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello Calendar");		
+		
+	}
+
+}
